80e2ef451f030bdcfb4029591e365481c2aa2b5d
El-DoctorDot <samdantasguedes19@gmail.com>
2025-03-08 17:02:16 -0300

    file update. This program takes the user's name and a number, calculates the square of that number, and displays the result on the screen, along with the user's name.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(squaredNumber, userName);
     }
-}
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult( int square, string name)
+    {
+        Console.WriteLine($" {name}, the square of your number is {square}");
+    }
+   
+}   
