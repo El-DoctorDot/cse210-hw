8eb4aa5d59403b54376b390d76127170f5453c4c
El-DoctorDot <samdantasguedes19@gmail.com>
2025-03-08 16:32:57 -0300

    File update. The program prompts the user to input a list of numbers, both positive and negative. It then calculates the sum and average of the numbers, identifies the largest number, and finds the smallest positive number in the list."

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+    {   
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a lits of numbers ( both positive and neagtive), type 0 when you are done");
+
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            if (number == 0)
+            {
+                break;
+                
+            }
+            else
+            {
+                numbers.Add(number);
+            }
+
+
+        }
+
+        if (numbers.Count > 0)
+        {
+            int sum = numbers.Sum();
+            double average = numbers.Average();
+            int max = numbers.Max();
+
+            var positiveNumbers = numbers.Where(n => n > 0);
+            int minPositive = positiveNumbers.Any() ? positiveNumbers.Min() : 0;
+
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The Average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+
+            if (minPositive > 0)
+            {
+                Console.WriteLine($"The smaller positive number is: {minPositive}");
+            }
+            else
+            {
+                Console.WriteLine("There are no positive numbers");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered");
+        }
     }
 }
