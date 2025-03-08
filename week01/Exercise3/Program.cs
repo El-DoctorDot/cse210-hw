e07575dbcda14591bbab426236efca5b3b081e0a
El-DoctorDot <samdantasguedes19@gmail.com>
2025-03-08 15:10:40 -0300

    File update. The program generates a random number between 1 and 100 and asks the user to guess it. After each guess, the program provides hints as to whether the number is higher or lower. If the user guesses correctly, he is asked if he wants to play again. If he chooses 'yes', the game restarts with a new number. If he chooses 'no', the program exits."

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,47 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {   
+
+        Random randomGeneretor = new Random();
+        string playAgain;
+
+        do
+        {
+            int magicNumber = randomGeneretor.Next(1, 100);
+            int guess = -1;
+        
+
+
+            while (guess != magicNumber)
+            {
+                Console.Write(" What is the magic number?  ");
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+
+                Console.Write(" What is your guess?");
+                string answer = Console.ReadLine();
+                guess = int.Parse(answer);
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else if (guess > magicNumber)
+                {   
+                    Console.WriteLine("Lower!");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            playAgain = Console.ReadLine();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Goodbye!");
     }
 }
