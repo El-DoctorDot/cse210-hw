84362eb2ec1071e1c999306c68294971bfdcb22f
El-DoctorDot <samdantasguedes19@gmail.com>
2025-03-10 19:22:00 -0300

    File update. The program creates class Resume and Job and adds the Display method in both codes.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+      Job job1 = new Job();
+      job1._companyName = "Microsoft";
+      job1._jobTitle = "Software Engineer";
+      job1._startYear = 2019;
+      job1._endYear = 2022;
+      
+      Job job2 = new Job();
+      job2._companyName = "Apple";
+      job2._jobTitle = "Manager";
+      job2._startYear = 2022;
+      job2._endYear = 2023;
+
+      job1.Display();
+      job2.Display();
+      
+
+
+      Resume myresume = new Resume();
+      myresume._name = "John Doe";
+
+
+      myresume._jobs.Add(job1);
+      myresume._jobs.Add(job2);
+      
+
+      myresume.Display();
+      
     }
+    
 }
